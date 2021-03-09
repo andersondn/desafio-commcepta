a10d75cb816eb29857e95858b0010efd084002fb
Anderson Nunes <andersonfngto@gmail.com>
2021-03-09 15:12:18 -0300

    Load json data and display

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,4 @@
 
-// const init = {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     mode: 'cors',
-//     cache: 'default'
-// }
-
-// let request = new Request(, init)
-
 function loadData() {
   return new Promise((resolve, reject) => {
     fetch('./data/dados.json')
@@ -21,32 +10,73 @@ function loadData() {
 
 }
 
-
 function showData(data) {
-  //  console.log(data)
-  var app = document.querySelector('#result');
-  app.innerHTML = '<ul>' + data.map(function (wizard) {
-    return '<li>' + wizard.nome + '</li>';
-  }).join('') + '</ul>';
+  const employeeGrid = document.querySelector('.employee-grid');
+  employeeGrid.innerHTML = data.map(employee => {
+    return `<a> 
+    <div data-employee-id="${employee.id}" class="employee-card">
+    <div class="employee-photo">
+      <img src="assets/img/${employee.foto}">
+      <div class="badge">${employee.id}</div>
+    </div>
+    <div class="details">
+      <div class="name">${employee.nome}</div>
+      <div class="role">${employee.cargo}</div>
+    </div>
+  </div>
+  </a>`
+  }).join('')
+
+}
+
+function update(event) {
+  event.preventDefault()
+console.log("fui clicado", event)
 }
 
 
+
+// Aguarda o carregamento da página 
 document.addEventListener("DOMContentLoaded", async () => {
-  const data = await loadData()
-  // showData(data)
-  console.log(data)
 
-
-  let navBarToggle = document.querySelector('.menu-toggle');
-  let mainNav = document.querySelector('.menu');
-
-  navBarToggle.addEventListener('click', function () {
-      console.log('toggle')
-      mainNav.classList.toggle('active');
+  ///
+  const navBarToggle = document.querySelector('.menu-toggle');
+  const mainNav = document.querySelector('.menu');
+  navBarToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
   });
 
 
 
+
+  /// Carrega arquivo JSON e exibe na tela
+  const employeeList = await loadData()
+  showData(employeeList)
+
+
+
+  //Processa os cliques nos cards
+  document.querySelectorAll('.employee-grid .employee-card').forEach(item => {
+    item.addEventListener('click', event => {
+      const employeeCard = event.target.closest(".employee-card")
+      const { employeeId } = employeeCard.dataset;
+      //Remove a classe "active" dos outros itens
+      event.stopPropagation()
+      document.querySelectorAll('.employee-card').forEach(item => {
+        item.classList.remove('active');
+      })
+
+      employeeCard.classList.add('active');
+
+    })
+  })
+
+
+
+
+
 });
+
+
 
 
