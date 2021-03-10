ce5bc09d78213fb0b867054c8aff7419b189c7ec
Anderson Nunes <andersonfngto@gmail.com>
2021-03-10 16:04:12 -0300

    Change card functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,21 @@
+// Aguarda o carregamento da página 
+document.addEventListener("DOMContentLoaded", async () => {
 
-function loadData() {
-  return new Promise((resolve, reject) => {
-    fetch('./data/dados.json')
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
+  function loadData() {
+    return new Promise((resolve, reject) => {
+      fetch('./data/dados.json')
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
 
-  })
+    })
 
-}
+  }
 
-function showData(data) {
-  const employeeGrid = document.querySelector('.employee-grid');
-  employeeGrid.innerHTML = data.map(employee => {
-    return `<a> 
+  function showData(data) {
+    const employeeGrid = document.querySelector('.employee-grid');
+    employeeGrid.innerHTML = data.map(employee => {
+      return `<a> 
     <div data-employee-id="${employee.id}" class="employee-card">
     <div class="employee-photo">
       <img src="assets/img/${employee.foto}">
@@ -25,25 +27,33 @@ function showData(data) {
     </div>
   </div>
   </a>`
-  }).join('')
+    }).join('')
 
-}
+  }
 
-function updateDetailsCard(employeeCard) {
-  const { employeeId } = employeeCard.dataset;
-  const detailCard = document.querySelector('.employee-details');
+  function updateDetailsCard(empoyeeData) {
+    const detailCard = document.querySelector('.employee-details');
 
     detailCard.classList.remove('animate-card');
     detailCard.offsetWidth;
     detailCard.classList.add('animate-card');
 
+    setTimeout(() => {
+      detailCard.querySelector('.employee-photo')
+        .innerHTML = `<img src="assets/img/${empoyeeData.foto}">`
+      detailCard.querySelector('.name')
+        .innerHTML = empoyeeData.nome
+      detailCard.querySelector('.role')
+        .innerHTML = empoyeeData.cargo
+      detailCard.querySelector('.age')
+        .innerHTML = empoyeeData.idade
+    }, 250)
 
-}
+  }
 
 
 
-// Aguarda o carregamento da página 
-document.addEventListener("DOMContentLoaded", async () => {
+
 
   ///
   const navBarToggle = document.querySelector('.menu-toggle');
@@ -53,31 +63,30 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
 
-
-
   /// Carrega arquivo JSON e exibe na tela
   const employeeList = await loadData()
   showData(employeeList)
-
-
 
   //Processa os cliques nos cards
   document.querySelectorAll('.employee-grid .employee-card').forEach(item => {
     item.addEventListener('click', event => {
       const employeeCard = event.target.closest(".employee-card")
       //Remove a classe "active" dos outros itens
-      event.stopPropagation()
       document.querySelectorAll('.employee-card').forEach(item => {
         item.classList.remove('active');
       })
-      updateDetailsCard(employeeCard)
+      const { employeeId } = employeeCard.dataset;
+      const [employeeData] = employeeList.filter(employee => employee.id == employeeId)
+
+      updateDetailsCard(employeeData)
       employeeCard.classList.add('active');
 
     })
   })
 
 
-
+  ///Ativa o primeiro card
+  document.querySelector(".employee-grid .employee-card").click()
 
 
 });
